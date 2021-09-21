d4fb18427bba4a24050770b991d1ef64307f6a84
developer one <vickyvijay151@gmail.com>
2021-09-21 13:36:30 +0530

    commit

---
+++ typescript.js
@@ -0,0 +1,4 @@
+This is a simple text file, text is ok 
+ls
+clear
+[D[D[A[A[A[A[D[D[D[D[B[B[B[C[[A[C[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
